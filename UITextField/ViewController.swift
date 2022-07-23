3e0626087e2220892dc242ab07f4979f5ba236d1
Dima <protsack.dima25@gmail.com>
2022-07-24 02:10:17 +0300

    TextField

--- UITextField/ViewController.swift
+++ UITextField/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var login: UITextField!
     var password = UITextField()
+    var validator = Validator()
+    var storage = Storage.initial
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,9 +51,10 @@ class ViewController: UIViewController {
 }
 
 
-//добовляем расширение чтобя проделегировать
+//Добовляем расширение чтобя проделегировать
 extension ViewController: UITextFieldDelegate {
     
+    //метод который указывет на то что после ведения логина и нажатия кнопки done нас автоматом перекидывает на строку password
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == login {
             password.becomeFirstResponder()
@@ -61,4 +64,57 @@ extension ViewController: UITextFieldDelegate {
         return true
     }
     
+    //Будет ли вводиться определееный текст
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        switch textField {
+        case login:
+            break
+            //не хочу чтобы что то вводилось кроме цифр
+        case password: return validator.hasNumbers(text: string)
+            
+        default: break
+        }
+        return true
+    }
+    
+    
+    //нельзя удалить текст,если мы редактируем другую строку,чтобы ее удалить надо обратно в нее вернуться
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        if textField.isEditing {
+            return true
+        }
+        return false
+    }
+    
+    //хотим сделать что при в воде логина и пароля они будут сохраняться локально и в дополненни выводиться в консоль
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == login {
+            storage.login = textField.text!
+        } else {
+            storage.password = textField.text!
+            print(storage.login, storage.password)
+        }
+    }
+    
+}
+    
+    
+    struct Validator {
+        let numbers = "123456789"
+        //проверяет что текст не содержит что то кроме цифр
+        func hasNumbers(text: String) -> Bool {
+            for oneNumber in numbers {
+                if text.contains(oneNumber) {return true}
+            }
+            return false
+        }
+    }
+    
+
+
+struct Storage {
+    var login = ""
+    var password = ""
+    
+    static let initial = Storage()
 }
